907c811fa9634bf3297e2e16551b3747ba88b3b0
objektwerks <objektwerks@runbox.com>
2025-03-28 14:58:06 -0400

    added menu

---
+++ src/main/scala/objektwerks/Menu.scala
@@ -0,0 +1,39 @@
+package objektwerks
+
+import scalafx.Includes._
+import scalafx.application.Platform
+import scalafx.event.ActionEvent
+import scalafx.scene.control.{Alert, Menu => MenuRoot, MenuBar, MenuItem, SeparatorMenuItem, TextArea}
+import scalafx.scene.control.Alert.AlertType
+import scalafx.scene.layout.GridPane
+
+final class Menu(context: Context) extends MenuBar:
+  val textAreaAbout = new TextArea():
+    editable = false
+    wrapText = true
+    text = context.aboutAlertContentText
+
+  val gridPaneAbout = new GridPane():
+    prefHeight = 120
+    prefWidth = 200
+  gridPaneAbout.add(textAreaAbout, 0, 0)
+
+  val menuItemAbout = new MenuItem:
+    text = context.menuAbout
+    onAction = (_: ActionEvent) =>
+      new Alert(AlertType.Information):
+        initOwner(App.stage)
+        title = context.windowTitle
+        headerText = context.aboutAlertHeaderText
+        dialogPane().content = gridPaneAbout
+      .showAndWait()
+
+  val menuItemExit = new MenuItem:
+    text = context.menuExit
+    onAction = (_: ActionEvent) => Platform.exit()
+
+  val menuRoot = new MenuRoot():
+    text = context.menuMenu
+    items = List(menuItemAbout, SeparatorMenuItem(), menuItemExit)
+
+  menus = List(menuRoot)
