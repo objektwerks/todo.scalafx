b8cc0d05c94bd60c596bf3575de3828b2b1ef4bb
objektwerks <objektwerks@runbox.com>
2025-03-28 15:02:35 -0400

    added menu to view

--- src/main/scala/objektwerks/View.scala
+++ src/main/scala/objektwerks/View.scala
@@ -4,10 +4,12 @@ import scalafx.scene.Scene
 import scalafx.scene.layout.VBox
 
 final class View(context: Context):
+  val menu = Menu(context)
+
   val vbox = new VBox:
     prefWidth = context.windowWidth
     prefHeight = context.windowHeight
-    children = List()
+    children = List(menu)
 
   val scene = new Scene:
     root = vbox
