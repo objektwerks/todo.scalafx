e257635b98d25d9f9b3e00265a991ee5a9f05660
objektwerks <objektwerks@runbox.com>
2025-03-28 14:54:31 -0400

    added app image to context

--- src/main/scala/objektwerks/Context.scala
+++ src/main/scala/objektwerks/Context.scala
@@ -2,9 +2,11 @@ package objektwerks
 
 import com.typesafe.config.Config
 
-import scalafx.scene.image.{Image, ImageView}
+import scalafx.scene.image.Image
 
 final class Context(config: Config):
   val windowTitle = config.getString("window.title")
   val windowWidth = config.getDouble("window.width")
   val windowHeight = config.getDouble("window.height")
+
+  def imageAppIcon = Image(Image.getClass.getResourceAsStream("/todo.png"))
