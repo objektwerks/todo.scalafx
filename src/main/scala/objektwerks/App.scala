a6fefceb900b2a9e449c3845a08115e730793db6
objektwerks <objektwerks@runbox.com>
2025-03-28 14:45:52 -0400

    refactoring app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,11 +1,14 @@
 package objektwerks
 
+import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
 import scalafx.application.JFXApp3
 
 object App extends JFXApp3 with LazyLogging:
   logger.info("Starting app ...")
+
+  val context = Context(ConfigFactory.load("app.conf"))
 
   override def start(): Unit =
     stage = new JFXApp3.PrimaryStage:
