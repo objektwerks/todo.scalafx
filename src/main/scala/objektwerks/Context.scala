091d092bedf73b41d558221f3c789144c4b3879d
objektwerks <objektwerks@runbox.com>
2025-03-28 14:43:47 -0400

    added context

---
+++ src/main/scala/objektwerks/Context.scala
@@ -0,0 +1,10 @@
+package objektwerks
+
+import com.typesafe.config.Config
+
+import scalafx.scene.image.{Image, ImageView}
+
+final class Context(config: Config):
+  val windowTitle = config.getString("window.title")
+  val windowWidth = config.getDouble("window.width")
+  val windowHeight = config.getDouble("window.height")
