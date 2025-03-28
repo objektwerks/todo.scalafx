ad5a32ce2fd659963dc06341884dbd1fb118e1ea
objektwerks <objektwerks@runbox.com>
2025-03-28 16:36:14 -0400

    enhanced toods

--- src/main/scala/objektwerks/ui/Todos.scala
+++ src/main/scala/objektwerks/ui/Todos.scala
@@ -1,5 +1,6 @@
 package objektwerks.ui
 
+import scalafx.scene.control.Label
 import scalafx.scene.layout.VBox
 
 import objektwerks.Store
@@ -7,3 +8,6 @@ import objektwerks.Store
 final class Todos(context: Context, store: Store) extends VBox:
   println(context)
   println(store)
+
+  val labelTodos = new Label:
+    text = context.menuAbout
