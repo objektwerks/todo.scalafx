90224769c7589447fa96e51cc5aac4fadabf6830
objektwerks <objektwerks@runbox.com>
2025-03-28 15:39:10 -0400

    refactored todo

--- src/main/scala/objektwerks/Todo.scala
+++ src/main/scala/objektwerks/Todo.scala
@@ -7,11 +7,12 @@ import upickle.default.ReadWriter as JsonSupport
 object Todo:
   given Ordering[Todo] = Ordering.by[Todo, String](t => t.created).reverse
 
+  def default(): Todo = Todo(id = 1, todo = "Drink beer!")
   def datetime(): String = Instant.now.toString
 
 @upickle.implicits.serializeDefaults(true)
 final case class Todo(id: Int,
-                      todo: String,
                       created: String = datetime(),
-                      completed: String = "") derives CanEqual, JsonSupport:
+                      completed: String = "",
+                      todo: String) derives CanEqual, JsonSupport:
   def file: String = s"$id.json"
